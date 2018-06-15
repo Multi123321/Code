065f5ce2b5663a2db59bf5d70aa1fe3ffeb40a79
Markus Kraft <Markus@Markus-iMac.local>
2018-06-15 08:56:04 +0200

    moved to seperate folder

--- C - LimitOrders/tests/Price_test.cpp
+++ C - LimitOrders/tests/Price_test.cpp
@@ -5,9 +5,29 @@
 #include <boost/test/output_test_stream.hpp>
 
 BOOST_AUTO_TEST_CASE(Price_constructor_test) {
-  Price price(10.2f, 15.2f, 20l);
+  Price price(10.2f, 15.123f, 20l);
 
   BOOST_CHECK_EQUAL(price.getBid(), 10.2f);
+  BOOST_CHECK_EQUAL(price.getAsk(), 15.123f);
+  BOOST_CHECK_EQUAL(price.getTime(), 20l);
+}
+
+BOOST_AUTO_TEST_CASE(Price_clone_test) {
+  Price price(10.123f, 50000.2f, 20l);
+
+   Price clone = price.clone();
+
+   BOOST_CHECK(&price != &clone);
+
+   BOOST_CHECK_EQUAL(price.getBid(), clone.getBid());
+   BOOST_CHECK_EQUAL(price.getAsk(), clone.getAsk());
+   BOOST_CHECK_EQUAL(price.getTime(), clone.getTime());
+}
+
+BOOST_AUTO_TEST_CASE(Price_getSpread_test) {
+  Price price(10.2f, 15.123f, 20l);
+
+  BOOST_CHECK_EQUAL(price.getSpread(), 10.2f);
 }
 
 // BOOST_AUTO_TEST_CASE(point_operator_test) {
