8d0f95c5373f4a21a1b23be47e895264b0f57a96
Markus Kraft <Markus@Markus-iMac.local>
2018-06-19 16:49:30 +0200

    turned generator into executable skript

--- InputData/generator.m
+++ InputData/generator.m
@@ -1,5 +1,9 @@
-function generator(mu, k, sigma,S0)
-% Call like this: generator(0, 0.00005, 0.002, 1)
+#!/Applications/Octave.app/Contents/Resources/usr/bin/octave-cli -qf
+
+mu = 0;
+k = 0.00005;
+sigma = 0.002;
+S0 = 1;
 
 %GENERATOR Summary of this function goes here
 %   Detailed explanation goes here
@@ -8,25 +12,26 @@ n = 1000000;           % Anzahl der Knicke/Tage
 T = (1:n);          
 M = 1;          % Anzahl der Pfade
 
-E = sigma * randn(1,n);
-S = S0 * ones(1,n);
+for i=1:4
+    disp(['Starting ' num2str(i)]);
 
-for j=2:n
-    S(j) = ((S(j-1))^(1-k))*exp(k*mu + E(j-1));
+    E = sigma * randn(1,n);
+    S = S0 * ones(1,n);
+
+    for j=2:n
+        S(j) = ((S(j-1))^(1-k))*exp(k*mu + E(j-1));
+    end;
+
+
+    %S = floor(S*10000)/10000;
+
+    out = zeros(n, 2);
+
+    out(:,1) = 0:200:200*(n-1);
+    out(:,2) = S;
+
+    csvwrite(['kurs' num2str(i) '.csv'], out);
+
+    disp([num2str(i) ' done']);
 end;
-
-
-%S = floor(S*10000)/10000;
-
-out = zeros(n, 2);
-
-out(:,1) = 0:200:200*(n-1);
-
-% disp(size(S));
-% disp(size(out));
-
-out(:,2) = S;
-
-csvwrite('kurs.csv', out);
-
 % plot (T,S);
