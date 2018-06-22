275593de73dcaa1706d354ae7251292f8ac304ab
Sperrus <esp765@gmx.de>
2018-06-22 12:24:42 +0200

    adjust runner.h to use avx

--- C_BasicTrader/src/Runner.h
+++ C_BasicTrader/src/Runner.h
@@ -3,38 +3,39 @@
 
 #include "PriceFeedData.h"
 #include <string>
+#include <x86intrin.h>
 
 class Runner
 {
 private:
 
 public:
-    double prevExtreme;
-    long prevExtremeTime;
+    __m256d prevExtreme;
+    __m256i prevExtremeTime;
 
-    double prevDC;
-    long prevDCTime;
+    __m256d prevDC;
+    __m256i prevDCTime;
 
-    double extreme;
-    long extremeTime;
+    __m256d extreme;
+    __m256i extremeTime;
 
-    double deltaUp;
-    double deltaDown;
-    double deltaStarUp, deltaStarDown;
-    double osL;
-    int type;
+    __m256d deltaUp;
+    __m256d deltaDown;
+    __m256d deltaStarUp, deltaStarDown;
+    __m256d osL;
+    __m128i type;
     bool initalized;
-    double reference;
+    __m256d reference;
 
     std::string fileName;
 
     Runner();
-    Runner(double threshUp, double threshDown, PriceFeedData::Price price, std::string file, double dStarUp, double dStarDown);
-    Runner(double threshUp, double threshDown, double price, std::string file, double dStarUp, double dStarDown);
-    Runner(double threshUp, double threshDown, std::string file, double dStarUp, double dStarDown);
+    Runner(__m256d threshUp, __m256d threshDown, PriceFeedData::Price price, std::string file, __m256d dStarUp, __m256d dStarDown);
+    Runner(__m256d threshUp, __m256d threshDown, double price, std::string file, __m256d dStarUp, __m256d dStarDown);
+    Runner(__m256d threshUp, __m256d threshDown, std::string file, __m256d dStarUp, __m256d dStarDown);
 
-    int run(PriceFeedData::Price price);
-    int run(double price);
+    __m128i run(PriceFeedData::Price price);
+    __m128i run(double price);
 
 
 };
