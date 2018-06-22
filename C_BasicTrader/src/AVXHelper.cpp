d5b2d09355bc58719a37413d61ee25d3943bef52
Markus Kraft <Markus@Markus-iMac.local>
2018-06-22 15:44:39 +0200

    initial commit

---
+++ C_BasicTrader/src/AVXHelper.cpp
@@ -0,0 +1,4 @@
+#include "AVXHelper.h"
+
+AVXHelper::AVXHelper()
+{}
---
+++ C_BasicTrader/src/AVXHelper.h
@@ -0,0 +1,13 @@
+#ifndef AVX_HELPER_H
+#define AVX_HELPER_H
+
+class AVXHelper
+{
+  private:
+    AVXHelper();
+
+  public:
+    // TODO
+};
+
+#endif
