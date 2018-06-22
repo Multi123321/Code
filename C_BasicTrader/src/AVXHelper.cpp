0d9ad8b3c0de4118b650a1bc287f040ff45d1002
Sperrus <esp765@gmx.de>
2018-06-22 16:40:53 +0200

    update avx helper

--- C_BasicTrader/src/AVXHelper.cpp
+++ C_BasicTrader/src/AVXHelper.cpp
@@ -1,4 +1,11 @@
 #include "AVXHelper.h"
+#include <x86intrin.h>
 
 AVXHelper::AVXHelper()
-{}
+{
+}
+
+__m256d avcLogDouble for (int i = 0; i < 4; i++)
+{
+    (double *)tmp[i] = log((double *)tmp[i]);
+}
--- C_BasicTrader/src/AVXHelper.h
+++ C_BasicTrader/src/AVXHelper.h
@@ -1,13 +1,22 @@
 #ifndef AVX_HELPER_H
 #define AVX_HELPER_H
 
+#include <x86intrin.h>
+
 class AVXHelper
 {
-  private:
-    AVXHelper();
+private:
+  AVXHelper();
 
-  public:
-    // TODO
+public:
+  static inline void AVXHelper::avxLogDouble(__m256d &input)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      (double *)&input[i] = log((double *)&input[i]);
+    }
+  }
+  // TODO
 };
 
 #endif
